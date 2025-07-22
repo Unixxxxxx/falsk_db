552a5ab93f217ad40662aa29342b83f6e3db9ffa
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-07-22 19:02:05 +0530

    add contact

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def Home():
     return render_template('index.html', name='Home')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', name='contact')
 
 if __name__=="__main__":
     app.run(debug=True)
